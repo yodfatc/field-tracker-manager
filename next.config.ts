4ead20a331ca370c48eb6e9943a53876540849ef
Yodfat <yc@yodfatc.com>
2026-02-04 08:51:57 +0200

    Add environment variable logging in next.config.ts
    - Introduced console logs to check the presence of Supabase URL and anon key in the environment.
    - This addition aids in debugging and ensures developers are aware of their configuration status during development.

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,11 @@ const nextConfig: NextConfig = {
 };
 
 export default nextConfig;
+console.log(
+  "ENV URL:",
+  process.env.NEXT_PUBLIC_SUPABASE_URL ? "YES" : "NO"
+);
+console.log(
+  "ENV KEY:",
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ? "YES" : "NO"
+);
